247ce50197345c0912fea9e4b605fe0cab1221b7
Nihat <basatemurnihat08@gmail.com>
2022-03-06 17:19:39 +0300

    Hi-Kod Proje

---
+++ Proje/console-programlama2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace console_programlama2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Adınızı Giriniz");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadınızı Girniz");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + name + " " + surname);
+        }
+    }
+}
