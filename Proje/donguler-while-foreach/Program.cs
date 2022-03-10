e96346cbebed1cddcb852903ddae618329d3d1fe
Nihat <basatemurnihat08@gmail.com>
2022-03-11 02:51:16 +0300

    donguler-while-foreach

---
+++ Proje/donguler-while-foreach/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace donguler_while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // While
+            // 1 den başlayarak console dan girilen sayıya kadar(sayıda dahil) ortalama hesaplayıp console a yazdıran program
+            Console.Write("Lütfen bir sayı giriniz : ");
+            int sayı = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac <= sayı)
+            {
+                toplam += sayac;
+                sayac ++;
+
+            }
+            Console.WriteLine(toplam/sayı);
+
+            // a dan z ye kadar olan tüm harfleri yazdır
+
+            char character = 'a';
+            while (character <= 'z')
+            {
+                Console.Write(character);
+                character ++;
+            }
+            
+            Console.WriteLine("**** FOREACH ****");
+            string[] arabalar = {"BMW","FORD","MERCEDES","TOYOTA"};
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
